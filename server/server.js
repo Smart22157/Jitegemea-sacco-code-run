39080e293ab1790933b5573cdcbc3fda9872ca03
Letsgooo125757 <jamesgitau429@gmail.com>
2025-02-04 22:02:33 +0300

    final

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-async function startServer() {
+async function startServer() { 
     try {
         console.log("Attempting to connect to the database..."); // Log before connecting
         await connectToDatabase(); // Attempt to connect to the database
